6b57a2819c1a53252e86c6b2c3952ac5bd1a507e
peteroostema <solrock72@gmail.com>
2016-09-23 15:19:23 -0400

    message

---
+++ app/src/main/java/edu/calvin/pno2/lab01/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.calvin.pno2.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,
+                "pno2", Toast.LENGTH_LONG).show();
+
+    }
+}
